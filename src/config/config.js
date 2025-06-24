3bbe335d8901290846d3cdd720a617f116b956d3
urquidijo <josueurquidicarrillo@gmail.com>
2025-06-24 16:54:33 -0400

    xd

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 
-//const API_URL = "https://backendsurtidor-production-a5f4.up.railway.app/api";
-const API_URL = "http://localhost:4000/api";
+const API_URL = "https://backendsurtidor-production-a5f4.up.railway.app/api";
+//const API_URL = "http://localhost:4000/api";
 export default API_URL;
